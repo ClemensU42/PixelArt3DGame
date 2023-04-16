502fdb2061457f0fdc39af906d11999bd411ff61
ClemensU42 <clemens_u@web.de>
2023-04-16 08:49:56 +0200

    Created basic project with OpenTK

---
+++ PixelArt3DGame/Game.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace PixelArt3DGame;
+
+public class Game : GameWindow
+{
+
+	public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+	{
+		
+	}
+
+	protected override void OnUpdateFrame(FrameEventArgs args)
+	{
+		base.OnUpdateFrame(args);
+
+		KeyboardState input = KeyboardState;
+
+		if (input.IsKeyDown(Keys.Escape))
+		{
+			Close();
+		}
+	}
+
+	protected override void OnRenderFrame(FrameEventArgs args)
+	{
+		base.OnRenderFrame(args);
+
+		GL.Clear(ClearBufferMask.ColorBufferBit);
+		
+		GL.ClearColor(0.5f, 0.3f, 0.2f, 1.0f);
+		
+		SwapBuffers();
+	}
+
+	protected override void OnResize(ResizeEventArgs e)
+	{
+		base.OnResize(e);
+		
+		GL.Viewport(0, 0, e.Width, e.Height);
+	}
+}
---
+++ PixelArt3DGame/main.cs
@@ -0,0 +1,21 @@
+﻿namespace PixelArt3DGame;
+
+static class MainClass
+{
+	static void Main()
+	{
+		try
+		{
+			using (Game game = new Game(640, 480, "Pixel art game"))
+			{
+				game.Run();
+				game.Dispose();
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+			Console.WriteLine(e.StackTrace);
+		}
+	}
+}
